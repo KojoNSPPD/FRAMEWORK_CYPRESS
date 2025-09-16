b17c8129fb288f258f14785e480b769179609042
kojoankra94 <120052998+kojoankra94@users.noreply.github.com>
2025-09-16 18:30:56 -0500

    first github actions commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl:
+      "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
     Url: "https://naveenautomationlabs.com/opencart/index.php?route=account/register",
-    
   },
 });
